4ba18a5271cc15cfb8d7910caff23b0a0f1a37c8
Alexander <kharchenko7717@gmail.com>
2020-05-22 15:45:58 +0300

    v1.4 (final)

--- src/main.js
+++ src/main.js
@@ -21,14 +21,7 @@ Vue.use(VueMeta)
 Vue.component('Loader', Loader)
 
 firebase.initializeApp({
-    apiKey: "AIzaSyA7lLQOhGjkOV3PnTN26hIkqWyDN6s7zm0",
-    authDomain: "cabura-1e48d.firebaseapp.com",
-    databaseURL: "https://cabura-1e48d.firebaseio.com",
-    projectId: "cabura-1e48d",
-    storageBucket: "cabura-1e48d.appspot.com",
-    messagingSenderId: "350588603394",
-    appId: "1:350588603394:web:2ad02178cddd2960566b1b",
-    measurementId: "G-9S2C70J5LW"
+    data 
 })
 
 let app;
