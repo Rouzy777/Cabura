97da2b9d5050d604f9e5bdc762df40df55164cf7
Alexander <kharchenko7717@gmail.com>
2020-06-02 14:46:33 +0300

    add demo screenshot

--- src/main.js
+++ src/main.js
@@ -21,7 +21,14 @@ Vue.use(VueMeta)
 Vue.component('Loader', Loader)
 
 firebase.initializeApp({
-    data 
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: "upwin-6ad20.firebaseapp.com",
+    databaseURL: "https://upwin-6ad20.firebaseio.com",
+    projectId: "upwin-6ad20",
+    storageBucket: "upwin-6ad20.appspot.com",
+    messagingSenderId: "1054627483087",
+    appId: "1:1054627483087:web:1005cc72a3db35081796ba",
+    measurementId: "G-L40PHPBJLF"
 })
 
 let app;
